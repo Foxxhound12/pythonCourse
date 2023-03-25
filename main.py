390acf932ab3ea81035c886eeafa9b049bed10b2
Foxxhound12 <128701923+Foxxhound12@users.noreply.github.com>
2023-03-25 18:21:17 +0000

    ....

---
+++ main.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+age_remain=90-int(age)
+age_r_mo=age_remain*12
+age_r_we=age_remain*52
+age_r_day=age_remain*365
+print(f"You have {age_r_day} days, {age_r_we} weeks and {age_r_mo} months left.")
+#Write your code below this line 👇
+#oder message=f"You have {age_r_day} days, {age_r_we} weeks and {age_r_mo} months left."
+# print(message)
