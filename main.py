f0a452933cd64d931bfc6843eb4bd25999574c73
Foxxhound12 <128701923+Foxxhound12@users.noreply.github.com>
2023-03-25 18:04:53 +0000

    Day1

---
+++ main.py
@@ -0,0 +1,3 @@
+# input() fragt nach Benutzer-Eingabe, nach eingabe wird ganze Funktion durch die Eingabe als String ersetzt
+#Hier wird input zuerst ausgeführt. Computer versucht print auszuführen, sieht input, pausiert print, führt input aus und führt dann die print Funktion weiter aus
+print("Hello"+input("What is your Name?"))
